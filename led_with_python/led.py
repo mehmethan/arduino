55b62da41de226b6df6c12933ab7c246e294b894
mehmet <mehmet-han@yandex.com>
2016-03-24 23:13:21 +0200

    python led on/of

--- led_with_python/led.py
+++ led_with_python/led.py
@@ -2,5 +2,5 @@ import serial
 
 serial_con = serial.Serial('/dev/ttyUSB0', 9600)
 while True:
-    command = raw_input("1.Open / 2.Close : ")
+    command = raw_input("1.On / 2.Off : ")
     serial_con.write(command)
